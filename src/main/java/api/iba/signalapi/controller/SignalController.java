32c9daf6d180a10265b9f0fb0f22280562d82af7
Thomas Geller <ThomasGeller@proton.me>
2025-04-06 19:24:16 +0200

    fin

--- src/main/java/api/iba/signalapi/controller/SignalController.java
+++ src/main/java/api/iba/signalapi/controller/SignalController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 import api.iba.signalapi.models.Signal;
 import api.iba.signalapi.repository.SignalRepository;
